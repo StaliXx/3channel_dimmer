ce08276c7a2977adb9116baf526de3230f40693c
Alexey <stalix@mail.ru>
2018-11-21 21:09:34 +0300

    гзв

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define dim1pin 4   // D2
+#define dim1pin 12  // D6
+#define dim1pin 14  // D5
+#define zeroPin 5   // D1
+
+volatile int tic, Dimmer;
+
+
+volatile byte interruptCounter = 0;
+int numberOfInterrupts = 0;
+
+void handleInterrupt() {
+  interruptCounter++;
 }
 
+void setup() {
+ 
+  Serial.begin(115200);
+  pinMode(zeroPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(zeroPin), handleInterrupt, FALLING);
+ 
+}
+ 
+
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
+  if(interruptCounter>0){
+ 
+      interruptCounter--;
+      numberOfInterrupts++;
+ 
+      Serial.print("An interrupt has occurred. Total: ");
+      Serial.println(numberOfInterrupts);
+  }
+ 
 }
